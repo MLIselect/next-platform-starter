e2599b2760d53c8012e6cf9067290b4598327f9d
MLIselect <daniel.lambrou.sales@gmail.com>
2026-01-25 19:32:10 -0500

    Update CheekyTicker.js

--- components/CheekyTicker.js
+++ components/CheekyTicker.js
@@ -1,28 +1,25 @@
 export default function CheekyTicker() {
   return (
-    <div className="w-full bg-blue-700 text-white overflow-hidden py-2 border-b border-blue-500 shadow-md relative z-50">
+    // CHANGED: bg-red-600 -> bg-blue-600 (Victory Blue)
+    <div className="w-full bg-blue-600 text-white overflow-hidden py-2 border-b border-blue-800 shadow-md relative z-50">
       <div className="whitespace-nowrap animate-marquee font-black text-xs md:text-sm tracking-widest uppercase flex gap-12">
-        {/* SECTION 1: K-12 VICTORY LAP */}
-        <span>🏆 GTHA SCHOOLS 100% CLOSED (TDSB, PEEL, YORK, DURHAM, HALTON)</span>
+        {/* SECTION 1: THE BIG WIN */}
+        <span>🏆 VICTORY: ALL GTHA SCHOOL BOARDS ARE CLOSED MONDAY</span>
         
-        {/* SECTION 2: THE NEW TARGET (UNIVERSITIES) */}
-        <span>👀 WATCHING UNIVERSITIES: U of T, TMU, YORK, MCMASTER... YOUR MOVE.</span>
-        <span>🎓 COLLEGES: DON'T MAKE STUDENTS COMMUTE IN THIS.</span>
+        {/* SECTION 2: THE NEW TARGETS */}
+        <span>👀 WATCHING UNIVERSITIES NOW: U OF T, YORK, TMU... YOUR MOVE.</span>
+        <span>🎓 COLLEGES: DON'T BE HEROES. CLOSE THE CAMPUS.</span>
         
-        {/* SECTION 3: EXAM INFO */}
-        <span>📉 HIGH SCHOOL EXAMS: CHECK YOUR BOARD SITE (MOST SHIFTED 1 DAY)</span>
-        
-        {/* SECTION 4: VIBES */}
-        <span>🛌 K-12 STUDENTS: YOU WON. GO TO SLEEP.</span>
-        <span>☕ UNI STUDENTS: WE ARE TRACKING YOUR STATUS NOW.</span>
+        {/* SECTION 3: USEFUL INFO */}
+        <span>📉 EXAM UPDATE: CHECK THE NEW "EXAM UPDATES" TAB BELOW</span>
+        <span>🛌 K-12 STUDENTS: TURN OFF ALARMS. GO TO SLEEP.</span>
         
         {/* REPEAT LOOP */}
-        <span>🏆 GTHA SCHOOLS 100% CLOSED (TDSB, PEEL, YORK, DURHAM, HALTON)</span>
-        <span>👀 WATCHING UNIVERSITIES: U of T, TMU, YORK, MCMASTER... YOUR MOVE.</span>
-        <span>🎓 COLLEGES: DON'T MAKE STUDENTS COMMUTE IN THIS.</span>
-        <span>📉 HIGH SCHOOL EXAMS: CHECK YOUR BOARD SITE (MOST SHIFTED 1 DAY)</span>
-        <span>🛌 K-12 STUDENTS: YOU WON. GO TO SLEEP.</span>
-        <span>☕ UNI STUDENTS: WE ARE TRACKING YOUR STATUS NOW.</span>
+        <span>🏆 VICTORY: ALL GTHA SCHOOL BOARDS ARE CLOSED MONDAY</span>
+        <span>👀 WATCHING UNIVERSITIES NOW: U OF T, YORK, TMU... YOUR MOVE.</span>
+        <span>🎓 COLLEGES: DON'T BE HEROES. CLOSE THE CAMPUS.</span>
+        <span>📉 EXAM UPDATE: CHECK THE NEW "EXAM UPDATES" TAB BELOW</span>
+        <span>🛌 K-12 STUDENTS: TURN OFF ALARMS. GO TO SLEEP.</span>
       </div>
     </div>
   );
